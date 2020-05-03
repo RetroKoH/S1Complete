43a6ff542dc5dc98b5e292c7c41cf68688d7c1c9
SonicRetroKingofHarts <cmgaudet2010@gmail.com>
2020-05-03 10:05:04 +0700

    Hard Reset
    There are bugs in the S1 Two-Eight base that weren't present in my last archived build so I'm going to start fresh

--- _maps/SS Walls.asm
+++
@@ -1,7 +0,0 @@
-; ---------------------------------------------------------------------------
-; Sprite mappings - Special Stage Walls - Uncompressed
-; ---------------------------------------------------------------------------
-Map_SSWalls:	dc.w SSBlock-Map_SSWalls
-SSBlock:	dc.b 1
-		dc.b $F0, $F, 0, 0, $F0
-		even
